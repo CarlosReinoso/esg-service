ab71c6efcf3ef6924634b832e4c1fb354857a43c
Carlos Reinoso <carlos.reinoso@myraindrop.co.uk>
2023-11-30 16:58:10 +0000

    script to add csv, connecto to database and add users table

--- app.js
+++ app.js
@@ -3,16 +3,18 @@ const app = express();
 const port = 3000;
 const mysql = require("mysql2");
 require("dotenv").config();
-// const importScript = require("./scripts/importScript");
+const importScript = require("./scripts/importScript");
 const createUsersTable = require("./database/createUsersTable");
 const selectTable = require("./database/selectTable");
-
 const deleteTable = require("./database/deleteTable");
+const createUsersCouponsTable = require("./database/createUsersCouponsTable");
 
 // deleteTable()
 
+// createUsersCouponsTable()
+
 // createUsersTable();
-selectTable()
+// selectTable()
 
 
 app.get("/", (req, res) => {
---
+++ database/createUsersCouponsTable.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const mysql = require('mysql2')
+const connection = mysql.createConnection(process.env.DATABASE_URL)
+
+async function createUsersCouponsTable() {
+  connection.connect((err) => {
+    if (err) {
+      console.error("Error connecting to database:", err);
+      return;
+    }
+    console.log("Connected to database");
+
+    const selectAllQuery = "SELECT * FROM users";
+
+    connection.query(selectAllQuery, (err, results) => {
+      if (err) {
+        console.error("Error selecting data:", err);
+      } else {
+        console.log("Selected data:", results);
+      }
+
+      connection.end();
+    });
+  });
+}
+
+module.exports = createUsersCouponsTable;
