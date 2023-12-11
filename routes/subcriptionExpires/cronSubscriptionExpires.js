da70eebec40253ad2b2182be10ad88bddee83711
Carlos Reinoso <carlos.reinoso@myraindrop.co.uk>
2023-12-11 16:53:19 +0000

    no emial to send today

--- routes/subcriptionExpires/cronSubscriptionExpires.js
+++ routes/subcriptionExpires/cronSubscriptionExpires.js
@@ -7,19 +7,23 @@ const subscriptionWillExpireEmail = require("../../emails/subscriptionWillExpire
 
 router.get("/cron-subscription-expires", async (req, res) => {
   try {
+    //Check if there are emails to send today
+    const [scubscriptionExpiresReminder] = await connection.query(
+      where1and2WeeksAway,
+      [currentDateOnly, currentDateOnly]
+    );
+
+    if (scubscriptionExpiresReminder.length == 0) {
+      return res.status(200).send("No emails to send today");
+    }
+
     // Check if the cron job has already been executed today
     const [statusRows] = await connection.query(
       "SELECT * FROM cron_execution_status WHERE executed_date = ?",
       [currentDateOnly]
     );
 
-    if (statusRows.length === 0) {
-      // The cron job hasn't been executed today, perform the job
-      const [scubscriptionExpiresReminder] = await connection.query(
-        where1and2WeeksAway,
-        [currentDateOnly, currentDateOnly]
-      );
-
+    if (statusRows.length === 0 && scubscriptionExpiresReminder.length != 0) {
       for (const student of scubscriptionExpiresReminder) {
         subscriptionWillExpireEmail(student.full_name, student.email);
       }
