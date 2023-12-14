cc4a7b958c07e5cfe2e096a8db67f49e74fc6685
Carlos Reinoso <carlos.reinoso@myraindrop.co.uk>
2023-12-14 21:38:10 +0000

    email text

--- emails/subscriptionWillExpireEmail.js
+++ emails/subscriptionWillExpireEmail.js
@@ -6,10 +6,9 @@ const subscriptionWillExpireEmail = async (name, to) => {
 
   const subject = "Tu beca de la ESG está próxima a vencer";
   const html = `
-        <p>Estimado ${name?.trim()},</p>
-        <p>Este correo es para comentarte que tu beca de la Enseñanza Saint Germain está por vencer, si dedeas continuar con este beneficio favor de contactar a soporte de la ESG por WhatsApp  para solicitar una renovación.</p>
+        <p>Estimado(a) ${name?.trim()}:</p>
+        <p>Los 6 meses de vigencia de tu Beca en la Enseñanza Saint Germain están próximos a vencer. Si deseas continuar con este beneficio, favor de contactar al WhatsApp de Soporte de la ESG (+52 1 55 6471 9461).</p>
         <p>¡Bendiciones Siempre!</p>
-        <p>ESG</p>
         `;
 
   transport.sendMail({ EMAIL, subject, to, html }, (err, _) => {
