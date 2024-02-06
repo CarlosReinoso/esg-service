fdb9037a468ba9e34640886eda719f1464b239d2
Carlos Reinoso <carlos.reinoso@myraindrop.co.uk>
2024-02-06 22:46:46 +0000

    oxxo remove comments

--- TODO.md
+++ TODO.md
@@ -3,4 +3,5 @@ watch https://www.youtube.com/watch?v=8ezSzH8jnY4 and implement oxxo payment
  - write the html
  - write the js https://github.com/stripe-samples/accept-a-payment/blob/main/custom-payment-flow/client/html/oxxo.js
  - write the server
+ - get pk key 
 phase 2 you can use a custom flow to add messaging
--- app-stripe.js
+++ app-stripe.js
@@ -46,14 +46,6 @@ app.get("/config", (req, res) => {
 app.post("/create-payment-intent", async (req, res) => {
   const { paymentMethodType, currency, paymentMethodOptions } = req.body;
 
-  // Each payment method type has support for different currencies. In order to
-  // support many payment method types and several currencies, this server
-  // endpoint accepts both the payment method type and the currency as
-  // parameters. To get compatible payment method types, pass
-  // `automatic_payment_methods[enabled]=true` and enable types in your dashboard
-  // at https://dashboard.stripe.com/settings/payment_methods.
-  //
-  // Some example payment method types include `card`, `ideal`, and `link`.
   const params = {
     payment_method_types:
       paymentMethodType === "link" ? ["link", "card"] : [paymentMethodType],
@@ -61,38 +53,6 @@ app.post("/create-payment-intent", async (req, res) => {
     currency: currency,
   };
 
-  // If this is for an ACSS payment, we add payment_method_options to create
-  // the Mandate.
-  if (paymentMethodType === "acss_debit") {
-    params.payment_method_options = {
-      acss_debit: {
-        mandate_options: {
-          payment_schedule: "sporadic",
-          transaction_type: "personal",
-        },
-      },
-    };
-  } else if (paymentMethodType === "customer_balance") {
-    params.payment_method_data = {
-      type: "customer_balance",
-    };
-    params.confirm = true;
-    params.customer =
-      req.body.customerId ||
-      (await stripe.customers.create().then((data) => data.id));
-  }
-
-  /**
-   * If API given this data, we can overwride it
-   */
-  if (paymentMethodOptions) {
-    params.payment_method_options = paymentMethodOptions;
-  }
-
-  // Create a PaymentIntent with the amount, currency, and a payment method type.
-  //
-  // See the documentation [0] for the full list of supported parameters.
-  //
   // [0] https://stripe.com/docs/api/payment_intents/create
   try {
     const paymentIntent = await stripe.paymentIntents.create(params);
