595e449b0037e287df503bfe3161de6510d5933d
Carlos Reinoso <carlos.reinoso@myraindrop.co.uk>
2024-02-25 18:50:47 +0000

    log

--- middleware/excludedEndpoints.js
+++ middleware/excludedEndpoints.js
@@ -1,6 +1,11 @@
 function excludedEndpoints(req) {
   const route = req.path;
+  console.log("🚀 ~ excludedEndpoints ~ route:", route);
   const excludedRoutesList = ["/stripe/oxxo-webhook"];
+  console.log(
+    "🚀 ~ excludedEndpoints ~ excludedRoutesList.includes(route):",
+    excludedRoutesList.includes(route)
+  );
   return excludedRoutesList.includes(route);
 }
 
