846d530f8ecf4e1c6c588b41c67d3656e330946a
Carlos Reinoso <carlos.reinoso@myraindrop.co.uk>
2024-02-26 20:11:30 +0000

    log webhook env

--- routes/stripe/oxxo.js
+++ routes/stripe/oxxo.js
@@ -9,58 +9,59 @@ const stripe = require("stripe")(
     : process.env.STRIPE_SECRET_KEY_DEV
 );
 
-router.post(
-  "/oxxo-webhook",
-  (req, res) => {
-    const sig = req.headers["stripe-signature"];
-    const payload = req.body;
+router.post("/oxxo-webhook", (req, res) => {
+  const sig = req.headers["stripe-signature"];
+  const payload = req.body;
+  console.log("🚀 ~ req.rawBody:", req.rawBody);
 
-    let event;
+  let event;
 
-    console.log("🚀 ~ process.env.STRIPE_WEBHOOK_SECRET:", process.env.STRIPE_WEBHOOK_SECRET)
-    try {
-      event = stripe.webhooks.constructEvent(
-        payload,
-        sig,
-        process.env.STRIPE_WEBHOOK_SECRET
-      );
-    } catch (err) {
-      // On error, log and return the error message
-      console.log(`Error message: ${err.message}`);
-      return res.status(400).send(`Webhook Error: ${err.message}`);
-    }
-
-    // Handle the event
-    //Jenny Rosen
-    // jr.succeed_immediately@example.com
-    switch (event.type) {
-      case "checkout.session.completed":
-        const checkoutSessionCompleted = event.data.object;
-        break;
-      case "checkout.session.async_payment_succeeded":
-        const checkoutSession = event.data.object;
-        const customerDetails = checkoutSession.customer_details;
-        const customerEmail = customerDetails.email;
-        const customerName = customerDetails.name;
-        oxxoCompletedWelcomeEmail(customerEmail, customerName);
-        break;
-      case "checkout.session.async_payment_failed":
-        // TODO email to admin and student of expired code
-        const checkoutSessionAsyncPaymentFailed = event.data.object;
-        console.log(
-          "🚀 ~ app.post ~ checkoutSessionAsyncPaymentFailed:",
-          checkoutSessionAsyncPaymentFailed
-        );
-        // Then define and call a function to handle the event checkout.session.async_payment_failed
-        break;
-
-      default:
-        console.log(`Unhandled event type ${event.type}.`);
-    }
-
-    // Return a response to acknowledge receipt of the event
-    res.json({ received: true });
+  console.log(
+    "🚀 ~ process.env.STRIPE_WEBHOOK_SECRET:",
+    process.env.STRIPE_WEBHOOK_SECRET
+  );
+  try {
+    event = stripe.webhooks.constructEvent(
+      payload,
+      sig,
+      process.env.STRIPE_WEBHOOK_SECRET
+    );
+  } catch (err) {
+    // On error, log and return the error message
+    console.log(`Error message: ${err.message}`);
+    return res.status(400).send(`Webhook Error: ${err.message}`);
   }
-);
+
+  // Handle the event
+  //Jenny Rosen
+  // jr.succeed_immediately@example.com
+  switch (event.type) {
+    case "checkout.session.completed":
+      const checkoutSessionCompleted = event.data.object;
+      break;
+    case "checkout.session.async_payment_succeeded":
+      const checkoutSession = event.data.object;
+      const customerDetails = checkoutSession.customer_details;
+      const customerEmail = customerDetails.email;
+      const customerName = customerDetails.name;
+      oxxoCompletedWelcomeEmail(customerEmail, customerName);
+      break;
+    case "checkout.session.async_payment_failed":
+      // TODO email to admin and student of expired code
+      const checkoutSessionAsyncPaymentFailed = event.data.object;
+      console.log(
+        "🚀 ~ app.post ~ checkoutSessionAsyncPaymentFailed:",
+        checkoutSessionAsyncPaymentFailed
+      );
+      // Then define and call a function to handle the event checkout.session.async_payment_failed
+      break;
+
+    default:
+      console.log(`Unhandled event type ${event.type}.`);
+  }
+
+  // Return a response to acknowledge receipt of the event
+  res.json({ received: true });
+});
 
 module.exports = router;
