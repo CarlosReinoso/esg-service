2ac8603e944b4ca12e329ce9dbf8aec383e55b33
Carlos Reinoso <jrp.carlos@hotmail.com>
2025-04-17 19:56:10 +0100

    keep alive

--- routes/public/index.js
+++ routes/public/index.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const status = require("./status");
 const oxxoWebhook = require("../stripe/oxxo.js");
+const keepAlive = require("./keep-alive.js");
 
 router.use("/", status);
+router.use("/keep-alive", keepAlive);
 router.use("/stripe", oxxoWebhook);
 
 module.exports = router;
